c9b581fe95420c9be089e0ff12ae0e6da84fed76
fiborges <filipaborges@outlook.pt>
2023-05-06 18:24:36 +0100

    inicializacao da sctruct minishell

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maricard <maricard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: filipa <filipa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 20:34:12 by maricard          #+#    #+#             */
-/*   Updated: 2023/04/21 20:41:47 by maricard         ###   ########.fr       */
+/*   Updated: 2023/05/06 18:06:26 by filipa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,38 @@
 
 // C libraries
 # include <stdio.h>
+
+typedef struct s_minishell_state
+{
+    char            *prompt;
+    char            *input;
+    int             status;
+    int             status_signal;
+    char            **environment_variables;
+    char            *pwd;
+    int             fd_in;
+    int             fd_out;
+    int             any_redirection;
+    int             is_interactive;
+    //struct termios  origin;
+    //struct termios  modified;
+    t_history_node          *history_head;
+    t_history_node          *history_index;
+    //t_terminal_control          *termc;
+}   t_minishell_state;
+
+typedef struct s_token
+{
+    char    *str;
+    int     token_type;
+}               t_token;
+
+typedef struct s_history_node
+{
+    void            *content;
+    struct s_hlist  *next;
+    struct s_hlist  *previous;
+}               t_history_node;
 
 // Functions
 // Utils
--- minishell/main.c
+++ minishell/main.c
@@ -1,8 +1,60 @@
 #include "minishell.h"
 
-int	main()
+t_minishell_state g_data;
+
+int	env_size(char **env)
 {
+	int	i;
+
+	i = 0;
+	while (env[i])
+		i++;
+	return (++i);
+}
+
+char	**env_dup(char **env)
+{
+	char	**env_array;
+	int		i;
+
+	i = 0;
+	env_array = malloc(sizeof(char *) * env_size(env));
+	if (!env_array)
+		exit(EXIT_FAILURE);
+	while (env[i])
+	{
+		env_array[i] = ft_strdup(env[i]);
+		i++;
+	}
+	env_array[i] = 0;
+	return (env_array);
+}
+
+
+void	minishell_init(t_minishell_state *shell, char **env)
+{
+	shell->prompt = "\x1b[32m[\x1b[33mMinishell\x1b[32m]~>\x1b[0m ";
+	shell->status = 0;
+	shell->status_signal = 0;
+	shell->input = NULL;
+	shell->environment_variables = env_dup(env);
+	if (!shell->environment_variables)
+		exit(EXIT_FAILURE);
+	shell->pwd = getcwd(NULL, 0);
+	shell->fd_in = 0;
+	shell->fd_out = 1;
+	shell->any_redirection = 1;
+	shell->history_head = NULL;
+	shell->history_index = NULL;
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
 	prompt();
+	minishell_init(&g_data, envp);
+
 	return (0);
 }
 
