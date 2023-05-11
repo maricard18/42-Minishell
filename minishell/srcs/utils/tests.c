c43a9b31f7cf0be96f97341ecff4c0be8380a798
Mario Cardoso Henriques <maricard@c1r15s3.42porto.com>
2023-05-11 11:16:57 +0100

    test

--- minishell/srcs/utils/tests.c
+++ minishell/srcs/utils/tests.c
@@ -6,7 +6,7 @@
 /*   By: maricard <maricard@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 10:29:23 by maricard          #+#    #+#             */
-/*   Updated: 2023/05/11 11:08:53 by maricard         ###   ########.fr       */
+/*   Updated: 2023/05/11 11:16:31 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,5 @@ void    lexer_test(t_token *token)
 		printf("[%s] ", token->args[i]);
 		i++;
 	}
-	printf("\n");
+	printf("\n\n");
 }
