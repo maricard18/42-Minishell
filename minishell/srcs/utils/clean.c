ed872403d5e536e9fe07fc83c089abc6ee714096
Mario Cardoso Henriques <maricard@c1r15s3.42porto.com>
2023-05-11 11:13:39 +0100

    updates

--- minishell/srcs/utils/clean.c
+++ minishell/srcs/utils/clean.c
@@ -6,7 +6,7 @@
 /*   By: maricard <maricard@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 10:45:38 by maricard          #+#    #+#             */
-/*   Updated: 2023/05/11 11:12:27 by maricard         ###   ########.fr       */
+/*   Updated: 2023/05/11 11:13:11 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void    clean_all(t_token *token)
     while (i <= token->n_tokens)
     {
         free(token->args[i]);
-        printf("freed token->args[%d]\n", i);
         i++;
     }
     free(token->args);
