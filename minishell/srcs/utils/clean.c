a94e9bc7085bae8e9169f493815323470e3df0c0
Mario Cardoso Henriques <maricard@c1r15s3.42porto.com>
2023-05-11 11:12:37 +0100

    updates

--- minishell/srcs/utils/clean.c
+++ minishell/srcs/utils/clean.c
@@ -6,12 +6,13 @@
 /*   By: maricard <maricard@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 10:45:38 by maricard          #+#    #+#             */
-/*   Updated: 2023/05/11 10:49:07 by maricard         ###   ########.fr       */
+/*   Updated: 2023/05/11 11:12:27 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// clean function for lexer
 void    clean_all(t_token *token)
 {
     int i;
