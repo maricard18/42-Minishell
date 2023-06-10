1f8e7c3b80bbcfe7b69a24d20e7f137ce13aa807
Mario Cardoso Henriques <maricard@c1r15s5.42porto.com>
2023-06-10 20:44:24 +0100

    syntax updated

--- minishell/srcs/parser/initialize.c
+++ minishell/srcs/parser/initialize.c
@@ -6,7 +6,7 @@
 /*   By: maricard <maricard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 19:07:47 by maricard          #+#    #+#             */
-/*   Updated: 2023/06/10 19:07:49 by maricard         ###   ########.fr       */
+/*   Updated: 2023/06/10 20:42:20 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	initialize_shell(char *str)
 	tokeniser(temp);
 	token = g_minishell.token;
 	//lexer_test();
-	if (validate_syntax(token))
+	if (validate_syntax(token) == 1)
 		return ;
 	//printf("       [" G "SYNTAX OK" RT "]\n");
 	parse_commands(0, 1, token);
--- minishell/srcs/parser/syntax.c
+++ minishell/srcs/parser/syntax.c
@@ -6,7 +6,7 @@
 /*   By: maricard <maricard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 14:09:49 by maricard          #+#    #+#             */
-/*   Updated: 2023/06/10 17:38:01 by maricard         ###   ########.fr       */
+/*   Updated: 2023/06/10 20:43:54 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,7 @@ int	validate_redirection_syntax(t_token *my_tokens)
 			tmp_tokens->type == HERE_DOC || tmp_tokens->type == APPEND)
 		{
 			if (!tmp_tokens->next || tmp_tokens->next->type != STRING)
-				return (handle_error(SYNTAX_ERROR,
-										"Missing filename after redirection"));
+				return (1);
 		}
 		tmp_tokens = tmp_tokens->next;
 	}
@@ -41,10 +40,9 @@ int	validate_pipe_syntax(t_token *my_tokens)
 		if (tmp_tokens->type == PIPE)
 		{
 			if (!tmp_tokens->prev || tmp_tokens->prev->type == PIPE)
-				return (handle_error(SYNTAX_ERROR, tmp_tokens->value));
+				return (1);
 			else if (!tmp_tokens->next)
-				return (handle_error(SYNTAX_ERROR,
-										"Incomplete command after operator"));
+				return (1);
 		}
 		tmp_tokens = tmp_tokens->next;
 	}
@@ -61,7 +59,7 @@ int	validate_allowed_tokens(t_token *my_tokens)
 		if (tmp_tokens->type != STRING && tmp_tokens->type != GREATER &&
 			tmp_tokens->type != APPEND && tmp_tokens->type != SMALLER &&
 			tmp_tokens->type != HERE_DOC && tmp_tokens->type != PIPE)
-			return (handle_error(SYNTAX_ERROR, "Unrecognized token"));
+			return (1);
 		tmp_tokens = tmp_tokens->next;
 	}
 	return (0);
@@ -69,30 +67,21 @@ int	validate_allowed_tokens(t_token *my_tokens)
 
 int	validate_syntax(t_token *my_tokens)
 {
-	int	error;
-	int	syntax_error;
-
-	error = 0;
-	syntax_error = 0;
-	error = validate_allowed_tokens(my_tokens);
-	if (error != 0)
+	if (validate_allowed_tokens(my_tokens) == 1)
 	{
-		syntax_error = 1;
-		handle_error(SYNTAX_ERROR, "Syntax error");
+		print_error(NULL, "minishell: syntax error near unexpected token\n", 2);
+		return (1);
 	}
-	error = validate_redirection_syntax(my_tokens);
-	if (error != 0)
+	if (validate_redirection_syntax(my_tokens) == 1)
 	{
-		syntax_error = 1;
-		handle_error(SYNTAX_ERROR, "Syntax error");
+		print_error(NULL, "minishell: syntax error near unexpected token\n", 2);
+		return (1);
 	}
-	error = validate_pipe_syntax(my_tokens);
-	if (error != 0)
+	if (validate_pipe_syntax(my_tokens) == 1)
 	{
-		syntax_error = 1;
-		handle_error(SYNTAX_ERROR, "Syntax error");
+		print_error(NULL, "minishell: syntax error near unexpected token '|'\n",
+				2);
+		return (1);
 	}
-	if (syntax_error)
-		return (-1);
 	return (0);
 }
