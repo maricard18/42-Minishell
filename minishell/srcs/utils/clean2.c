d5011d14996c81af7bda295aa884da093baf78e6
Mario Cardoso Henriques <maricard@c1r14s1.42porto.com>
2023-06-28 18:35:07 +0100

    final fixes

--- minishell/srcs/utils/clean2.c
+++ minishell/srcs/utils/clean2.c
@@ -6,7 +6,7 @@
 /*   By: maricard <maricard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 19:56:00 by maricard          #+#    #+#             */
-/*   Updated: 2023/06/27 19:05:28 by maricard         ###   ########.fr       */
+/*   Updated: 2023/06/28 18:34:00 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ void	clean_fd_list(t_fd *fd)
 	t_file		*file;
 	t_fd		*tmp;
 	int			i;
+	int			flag;
 
 	i = -1;
+	flag = 1;
 	g_minishell.fd = fd;
 	temp = g_minishell.parsed;
 	while (temp[++i])
@@ -28,12 +30,14 @@ void	clean_fd_list(t_fd *fd)
 		file = temp[i]->file;
 		while (file)
 		{
+			flag = 0;
 			tmp = fd->next;
 			free(fd);
 			fd = tmp;
 			file = file->next;
 		}
 	}
+	free(fd);
 	g_minishell.fd = NULL;
 }
 
